91b0ce33843073107eaa37b8b60ba7827e88d097
NicolasStorti <nicolasstorti00@gmail.com>
2025-02-20 20:57:26 -0300

    Project initial

---
+++ src/Controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Clickloc\Projeto\Controllers;
+
+interface Controller
+{
+    public function req():void;
+}
---
+++ src/Controllers/HomeViewController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Clickloc\Projeto\Controllers;
+
+class HomeViewController implements Controller
+{
+    public function req(): void
+    {
+
+    }
+}
